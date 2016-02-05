4186c9f4f9a79bc508bdc81443c0d77766717818
Stuart Gorman <sgorman@catsone.com>
2016-02-05 11:05:17 -0600

    add ssi

--- thingbits/examples/receiver/receiver.ino
+++ thingbits/examples/receiver/receiver.ino
@@ -1,6 +1,7 @@
 #include "VirtualWire.h"
 
 int pin_LED_POWER = 13;
+int pin_SIGNAL_STRENGTH = 7;
 
 void setup() {
   pinMode(pin_LED_POWER, OUTPUT);
@@ -18,6 +19,9 @@ void loop() {
   uint8_t bufferLength = VW_MAX_MESSAGE_LEN;
   
   if (vw_have_message()) {
+    Serial.print(getSignalStrength());
+    Serial.print("%~");
+    
     digitalWrite(pin_LED_POWER, HIGH);
     
     vw_get_message(readBuffer, &bufferLength);
@@ -29,4 +33,17 @@ void loop() {
     
     digitalWrite(pin_LED_POWER,LOW);
   }
-} 
+}
+
+int getSignalStrength() {
+  int rawSignalStrenth = analogRead(pin_SIGNAL_STRENGTH);
+  if (rawSignalStrenth < 151) {
+    return 0;
+  }
+  if (rawSignalStrenth > 399) {
+    return 100;
+  }
+
+  return int(floor(float(rawSignalStrenth - 150) / 2.5));
+}
+
