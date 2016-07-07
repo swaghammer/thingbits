32cc13feaae15122c074c100423093e021a348d0
Marlyn <marlyn@thingbits.com>
2016-07-07 15:08:16 -0500

    Create transmitTemperature.ino

---
+++ src/sensors/temperature/transmitTemperature/transmitTemperature.ino
@@ -0,0 +1,29 @@
+char* sensorType = "TMP";
+char* myID = "MARKET";
+int heartbeatInterval = 3600; // seconds
+int periodicReadingInterval = 600; // seconds, 0 for no periodic reading
+float CalVal= -0.75; //calibration offset value 
+
+#include "initialize.h"
+
+void setup() {
+  initializeSensor();
+  processEvent();
+}
+
+void loop() {
+  sleepUntilEvent();
+
+  processEvent();
+}
+
+void processEvent() {
+  int pinThermometer = 0;
+  char units[6] = "F";
+  char message[14]; 
+  float Temp=readTemp(pinThermometer)+CalVal;
+  dtoa(Temp, message, 0);
+  strcat(message, units);
+  
+  sendPeriodic(message);
+}
